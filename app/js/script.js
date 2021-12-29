9b3cabf32ca753e4ec69864a75af5885cdecb527
Jimmy Camus <jeremy.camus.varela@gmail.com>
2021-12-29 15:57:45 -0300

    Footer integration.

--- app/js/script.js
+++ app/js/script.js
@@ -19,5 +19,18 @@ const navSlide = () =>{
     });
 }
 
+function myFunction() {
+    const nav = document.querySelector('.nav');
+    const sticky = nav.offsetTop;
+    
+    if (window.pageYOffset >= sticky) {
+        nav.classList.add("sticky")
+    } else {
+        nav.classList.remove("sticky");
+    }
+}
 
+window.onscroll = function() {myFunction()};
 navSlide();
+
+
